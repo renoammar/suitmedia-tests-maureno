8da4e698cdecb4e87e3485ab4292a1a17bb67e57
Maureno Ammar <maurenoammar4@gmail.com>
2025-05-25 16:17:57 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,47 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://suitmedia-backend.suitdev.com",
+//         changeOrigin: true,
+//         secure: true,
+//       },
+//       "/storage": {
+//         target: "https://assets.suitdev.com",
+//         changeOrigin: true,
+//         secure: true,
+//       },
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://suitmedia-backend.suitdev.com",
-        changeOrigin: true,
-        secure: true,
-      },
-      "/storage": {
-        target: "https://assets.suitdev.com",
-        changeOrigin: true,
-        secure: true,
-      },
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy:
+        command === "serve"
+          ? {
+              "/api": {
+                target: "https://suitmedia-backend.suitdev.com",
+                changeOrigin: true,
+                secure: true,
+              },
+              "/storage": {
+                target: "https://assets.suitdev.com",
+                changeOrigin: true,
+                secure: true,
+              },
+            }
+          : undefined,
     },
-  },
+  };
 });
